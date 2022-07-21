9c61153e5b7624905aed9730aacdd9f34a023f72
ramiinberg <rami.inberg@gmail.com>
2022-07-21 13:51:09 +0300

    randomize numbers to screen when loading app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
+import { useState } from 'react'
+
 import './App.css'
 import Dice from './components/Dice'
 
 function App() {
+  const allNewDice = (diceCount = 10) => {
+    const diceArray = [...Array(diceCount).keys()]
+    return diceArray.map(id => ({
+      dieKey: id,
+      dieNumber: Math.floor(Math.random() * 6) + 1
+    }))
+  }
+
+  const [diceArray, setDiceArray] = useState(allNewDice())
+
+  const diceElements = diceArray.map(die => (
+    <Dice key={die.dieKey} value={die.dieNumber} />
+  ))
+
   return (
     <main>
-      <div className='container'>
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-      </div>
+      <div className='container'>{diceElements}</div>
     </main>
   )
 }
